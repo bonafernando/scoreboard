b9f3c1756f5121b2df4b0f1137cdc2297c1b7001
Fernando W. Bona <bonafernando@gmail.com>
2019-08-23 11:26:04 -0300

    rails g model Player name:string category:references && rails generate active_admin:resource player

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_23_140943) do
+ActiveRecord::Schema.define(version: 2019_08_23_142344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,4 +47,13 @@ ActiveRecord::Schema.define(version: 2019_08_23_140943) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_players_on_category_id"
+  end
+
+  add_foreign_key "players", "categories"
 end
