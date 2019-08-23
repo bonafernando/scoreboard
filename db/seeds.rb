9f3eff303e7e09b90ca7664421daad6dccc99a3c
Fernando W. Bona <bonafernando@gmail.com>
2019-08-23 11:17:42 -0300

    rails g model Category name:string && rails generate active_admin:resource category and update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+categories = %w{
+  Feminino
+  Masculino
+  Time\ Feminino
+  Time\ Masculino
+  Time\ Misto
+  Feminino\ Sênior
+  Masculino\ Sênior
+}
+categories.each do |c|
+  Category.create!(name: c)
+end
