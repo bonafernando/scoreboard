54d0205b89203c0a10a0eb048dfed23ddc64f499
Fernando W. Bona <bonafernando@gmail.com>
2019-08-23 18:37:23 -0300

    Set position, sum points and time. Also improve to only one query

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,13 @@
 class Player < ApplicationRecord
   belongs_to :category
-  has_many :scores
+  has_many :scores, -> { includes :wods }
   has_many :wods, through: :scores
+
+  def points
+    scores.sum(&:points)
+  end
+
+  def time
+    scores.sum(&:time) || Time.new(0)
+  end
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -1,4 +1,6 @@
 class Score < ApplicationRecord
   belongs_to :player
   belongs_to :wod
+
+  default_scope { includes(:player, :wod).order(:created_at) }
 end
